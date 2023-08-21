786f0cdabe7d2a016ff14c86ede52c0e75eb9d2b
MahabirGupta <mahabirg81@gmail.com>
2023-08-21 18:02:47 +0800

    Completed the Guess Game challenge

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -1,8 +1,77 @@
 public class GuessGame {
 
-    String player1;
-    String player2;
-    String player3;
+    String player1 = "John";
+    String player2 = "Tom";
+    String player3 = "Harry";
+    int generateNumber;
+    int returnGuessNumber;
     public void startGame() {
+        Player player = new Player();
+        generateNumber = (int)(Math.random() * 10);
+        System.out.println("Generated Number: " + generateNumber);
+        System.out.print(player1 + " make a guess:");
+        returnGuessNumber = player.guess();
+        System.out.println("Guessed Number: " + returnGuessNumber);
+
+        if (returnGuessNumber == generateNumber){
+            System.out.println(player1 + " you win");
+        }else {
+            while (returnGuessNumber != generateNumber){
+                System.out.println(player1 + " you lose");
+
+                generateNumber = (int)(Math.random() * 10);
+                System.out.println("Generated Number: " + generateNumber);
+                System.out.print(player2 + " make a guess:");
+                returnGuessNumber = player.guess();
+                System.out.println("Guessed Number: " + returnGuessNumber);
+
+                if (returnGuessNumber == generateNumber){
+                    System.out.println(player2 + " you win");
+                }
+                else {
+                    System.out.println(player2 + " you lose");
+
+                    generateNumber = (int)(Math.random() * 10);
+                    System.out.println("Generated Number: " + generateNumber);
+                    System.out.print(player3 + " make a guess:");
+                    returnGuessNumber = player.guess();
+                    System.out.println("Guessed Number: " + returnGuessNumber);
+
+                    if (returnGuessNumber == generateNumber){
+                        System.out.println(player3 + " you win");
+                    }
+                    else {
+
+                        System.out.println(player3 + " you lose");
+
+                        generateNumber = (int)(Math.random() * 10);
+                        System.out.println("Generated Number: " + generateNumber);
+                        System.out.print(player1 + " make a guess:");
+                        returnGuessNumber = player.guess();
+                        System.out.println("Guessed Number: " + returnGuessNumber);
+
+                        if (returnGuessNumber == generateNumber){
+                            System.out.println(player1 + " you win");
+                        }
+//                        else {
+//                            System.out.println(player1 + " you lose");
+//
+//                            generateNumber = (int)(Math.random() * 10);
+//                            System.out.println("Generated Number: " + generateNumber);
+//                            System.out.print(player2 + " make a guess:");
+//                            returnGuessNumber = player.guess();
+//                            System.out.println("Guessed Number: " + returnGuessNumber);
+//                            if (returnGuessNumber == generateNumber){
+//                                System.out.println(player2 + " you win");
+//                            }else {
+//
+//                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
     }
-}
+//}
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class Player {
+    public int guess() {
+        Scanner scanner = new Scanner(System.in);
+        int guessNumber = scanner.nextInt();
+        return guessNumber;
+    }
 }
